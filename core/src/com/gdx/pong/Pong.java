95bf727c07fe13ec33703d6027d07356dedfc8db
Eduardo <massa-messo@hotmail.com>
2017-08-23 04:58:58 +0200

    ball bouncing whithin the screen

---
+++ core/src/com/gdx/pong/Pong.java
@@ -0,0 +1,47 @@
+package com.gdx.pong;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+
+public class Pong extends ApplicationAdapter {
+	private OrthographicCamera camera;
+	private ShapeRenderer shapeRenderer;
+	private Vector2 direction;
+	private Vector2 position;
+
+	@Override
+	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false);
+		shapeRenderer = new ShapeRenderer();
+		direction = new Vector2(5f,5f);
+		position = new Vector2(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(255,255,255,1);
+		shapeRenderer.circle(position.x,position.y, 20);
+		shapeRenderer.end();
+		if(position.x >= Gdx.graphics.getWidth() || position.x <= 0)
+			direction.x = -direction.x;
+		if (position.y >= Gdx.graphics.getHeight() || position.y <= 0 )
+			direction.y = -direction.y;
+		position.add(direction);
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
