1ccbf7ebe68f0e2bc5185d364b3d2d59f9bbdc88
Eduardo <massa-messo@hotmail.com>
2017-09-16 22:25:32 +0200

    Fixed collision detection bug

--- core/src/com/gdx/pong/players/AIPlayer.java
+++ core/src/com/gdx/pong/players/AIPlayer.java
@@ -4,20 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.gdx.pong.Ball;
 import com.gdx.pong.main.Pong;
 
-public class AIPlayer  extends Player {
+public class AIPlayer extends Player {
 
     private static Ball ball;
-
 
     public AIPlayer() {
         super(4.35f);
         ball = Pong.getBall();
     }
 
-    public void move (){
-        if (isBallOnAIPlayerSide() && isBallComingToAIPLayer()) {
+    public void move() {
+        if (isBallOnAIPlayerSide() && isBallComingToPlayer(ball)) {
             if (isBallAboveMiddle()) {
-                if(isUpEdgeInsideScreen())
+                if (isUpEdgeInsideScreen())
                     moveUp();
             } else if (isBallBelowMiddle()) {
                 if (isDownEdgeInsideScreen())
@@ -27,22 +26,11 @@ public class AIPlayer  extends Player {
     }
 
     private boolean isBallOnAIPlayerSide() {
-        return (Math.abs((position.x + 160 ) - ball.getX()) < Gdx.graphics.getWidth()/2);
+        return (Math.abs((position.x + 160) - ball.getX()) < Gdx.graphics.getWidth() / 2);
     }
 
-    private boolean isBallComingToAIPLayer() {
-        if(isLeftPlayer)
-            return ball.getAbscissaDirection() < 0;
-        else
-            return ball.getAbscissaDirection() > 0;
-    }
+    private boolean isBallAboveMiddle() { return ball.getY() > (getY() + getHeight() / 2); }
 
-    private boolean isBallAboveMiddle(){
-        return ball.getY() > (getY() + getHeight() / 2);
-    }
-
-    private boolean isBallBelowMiddle(){
-        return ball.getY() < (getY() + getHeight() / 2);
-    }
+    private boolean isBallBelowMiddle() { return ball.getY() < (getY() + getHeight() / 2); }
 
 }
--- core/src/com/gdx/pong/players/Player.java
+++ core/src/com/gdx/pong/players/Player.java
@@ -1,80 +1,78 @@
 package com.gdx.pong.players;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.gdx.pong.Ball;
 
 
 public abstract class Player {
-    protected Rectangle position;
-    protected final boolean isLeftPlayer;
     private static boolean isFirstInstance = true;
+    protected final boolean isLeftPlayer;
     protected final float MOVEMENT;
+    protected Rectangle position;
 
 
-    protected Player(float MOVEMENT){
+    protected Player(float MOVEMENT) {
         int width = 20;
         int height = 100;
         if (isFirstInstance) {
             isLeftPlayer = true;
             isFirstInstance = false;
-        }
-        else
+        } else
             isLeftPlayer = false;
-        if(isLeftPlayer)
-            position =  new Rectangle(150, Gdx.graphics.getHeight()/2, width, height);
+        if (isLeftPlayer)
+            position = new Rectangle(150, Gdx.graphics.getHeight() / 2, width, height);
         else
-            position = new Rectangle( Gdx.graphics.getWidth() - 150, Gdx.graphics.getHeight()/2, width, height);
+            position = new Rectangle(Gdx.graphics.getWidth() - 150, Gdx.graphics.getHeight() / 2, width, height);
         this.MOVEMENT = MOVEMENT;
     }
 
 
     public boolean isCollisonDetected(Ball ball) {
-        return Intersector.overlaps(ball.getPosition(), position);
+        return isBallOrdinateWithinPaddleHeight(ball) && isBallComingToPlayer(ball) &&
+               isBallAbcissaWithinPaddleWidth(ball);
     }
 
-    public float getX(){
-        return position.x;
+    protected boolean isBallComingToPlayer(Ball ball) {
+        return (isLeftPlayer) ? ball.getAbscissaDirection() < 0 : ball.getAbscissaDirection() > 0;
     }
 
-    public float getY(){
-        return position.y;
+    private boolean isBallAbcissaWithinPaddleWidth(Ball ball) {
+        if (isLeftPlayer)
+            return ball.getX() <= (position.x + position.width) && ball.getX() > position.x;
+        else
+            return ball.getX() >= position.x && ball.getX() < (position.x + position.width);
+
     }
+
+    private boolean isBallOrdinateWithinPaddleHeight(Ball ball) {
+        return ball.getY() >= position.y && ball.getY() <= (position.y + position.height);
+    }
+
+    public float getX() { return position.x; }
+
+    public float getY() { return position.y; }
 
     public abstract void move();
 
-    public boolean isBallBehind(Ball ball){
+    public boolean isBallBehind(Ball ball) {
         int margin = 80;
         if (isLeftPlayer)
-            return (ball.getX() + margin ) < position.x;
+            return (ball.getX() + margin) < position.x;
         else
             return (ball.getX() - margin) > position.x;
     }
 
-    protected void moveUp(){
-        position.y += MOVEMENT;
-    }
+    protected void moveUp() { position.y += MOVEMENT; }
 
-    protected void moveDown(){
-        position.y -= MOVEMENT;
-    }
+    protected void moveDown() { position.y -= MOVEMENT; }
 
+    protected boolean isUpEdgeInsideScreen() { return (position.y + getHeight()) < Gdx.graphics.getHeight(); }
 
-    protected boolean isUpEdgeInsideScreen(){
-        return (position.y + getHeight()) < Gdx.graphics.getHeight();
-    }
+    protected boolean isDownEdgeInsideScreen() { return position.y > 0; }
 
-    protected boolean isDownEdgeInsideScreen(){
-        return position.y > 0;
-    }
+    public float getWidth() { return position.width; }
 
-    public float getWidth(){
-        return position.width;
-    }
-
-    public float getHeight() {
-        return position.height;
-    }
+    public float getHeight() { return position.height; }
 
 }
